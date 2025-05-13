c8827bde794cca8197e4d5f357c0e6e985a15111
haku7a <v.a.neyaskin@gmail.com>
2025-05-13 07:38:10 +0300

    feat(ads): add ad update view

--- ads/urls.py
+++ ads/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ad_list_view, ad_create_view
+from .views import ad_list_view, ad_create_view, ad_update_view
 
 app_name = 'ads'
 
 urlpatterns = [
     path('', ad_list_view, name='ad_list'),
     path('create/', ad_create_view, name='ad_create'),
-    path('ad/<int:pk>/edit/', ad_create_view, name='ad_update'),
+    path('ad/<int:pk>/edit/', ad_update_view, name='ad_update'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect, get_object_or_404 
+from django.http import HttpResponseForbidden
 from .models import Ad
 from .forms import AdForm
 
@@ -19,3 +20,23 @@ def ad_create_view(request):
     else:
         form = AdForm()
     return render(request, 'ads/ad_form.html', {'form': form})
+
+@login_required
+def ad_update_view(request, pk):
+    ad = get_object_or_404(Ad, pk=pk)
+    if ad.user != request.user:
+        return HttpResponseForbidden("Вы не можете редактировать это объявление.")
+    if request.method == 'POST':
+        form = AdForm(request.POST, request.FILES or None, instance=ad)
+        if form.is_valid():
+            form.save()
+            return redirect('ads:ad_list')
+    else:
+        form = AdForm(instance=ad)
+
+        context = {
+        'form': form,
+        'page_title': f'Редактировать: {ad.title}',
+        'ad': ad,
+        }
+    return render(request, 'ads/ad_form.html', context)
