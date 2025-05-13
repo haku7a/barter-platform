0942703af12de2c1715b1433dcb13eb7236507d2
haku7a <v.a.neyaskin@gmail.com>
2025-05-13 23:16:27 +0300

    feat(ads): add ExchangeProposalForm with user-specific ad_sender

--- ads/forms.py
+++ ads/forms.py
@@ -1,7 +1,21 @@
 from django import forms
-from .models import Ad
+from .models import Ad, ExchangeProposal
 
 class AdForm(forms.ModelForm):
     class Meta:
         model = Ad
         fields = ['title', 'description', 'image_url', 'category', 'condition']
+
+class ExchangeProposalForm(forms.ModelForm):
+    class Meta:
+        model = ExchangeProposal
+        fields = ['ad_sender', 'comment']
+        widgets = {
+            'comment': forms.Textarea(attrs={'rows': 3}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['ad_sender'].queryset = Ad.objects.filter(user=user)
