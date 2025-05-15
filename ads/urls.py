7efbc95c87fb3b8feb6f918e41f30c77f1916cfc
haku7a <v.a.neyaskin@gmail.com>
2025-05-15 15:51:08 +0300

    feat(ads): add url exchange_proposal_update_status

--- ads/urls.py
+++ ads/urls.py
@@ -4,7 +4,8 @@ from .views import (ad_list_view,
                     ad_update_view, 
                     ad_delete_view, 
                     exchange_proposal_create_view,
-                    exchange_proposal_list_view,)
+                    exchange_proposal_list_view,
+                    update_exchange_proposal_status_view,)
 
 app_name = 'ads'
 
@@ -15,4 +16,5 @@ urlpatterns = [
     path('ad/<int:pk>/delete/', ad_delete_view, name='ad_delete'),
     path('ad/<int:ad_receiver_pk>/propose/', exchange_proposal_create_view, name='exchange_proposal_create'),
     path('proposals/', exchange_proposal_list_view, name='exchange_proposal_list'),
+    path('proposals/<int:proposal_pk>/status/<str:new_status>/', update_exchange_proposal_status_view, name='exchange_proposal_update_status'),
 ]
